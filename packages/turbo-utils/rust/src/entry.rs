29afc218dd7dfec8c6102eb433ff370fb1ec0493
PiquoInterest <piquo@interesting.money>
2026-08-31 11:45:43 +0200

    feat: Export Rust package manager APIs

--- packages/turbo-utils/rust/src/entry.rs
+++ packages/turbo-utils/rust/src/entry.rs
@@ -3,6 +3,7 @@ mod legacy;
 
 mod config;
 mod json5;
+mod managers;
 mod root;
 
 pub use config::{
@@ -12,4 +13,11 @@ pub use config::{
 };
 pub use json5::{Json5Error, parse_json5};
 pub use legacy::*;
+pub use managers::{
+    MANAGER_COMMAND_TIMEOUT, MAX_MANAGER_CONFIG_BYTES, MAX_MANAGER_OUTPUT_BYTES, ManagerCommand,
+    ManagerCommandRunner, ManagerDetectionOptions, PackageManagers, SystemManagerCommandRunner,
+    get_available_package_managers, get_available_package_managers_with,
+    get_package_managers_bin_paths, get_package_managers_bin_paths_with, parse_manager_version,
+    resolve_executable_in_path,
+};
 pub use root::{TurboRootOptions, clear_turbo_root_cache, get_turbo_root};
